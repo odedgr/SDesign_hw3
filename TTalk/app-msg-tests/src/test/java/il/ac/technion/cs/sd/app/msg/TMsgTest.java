1b7ed83a10238b44813ed53c7b6746548ebaab2e
Brusi <bbrusi@gmail.com>
2015-05-24 13:10:50 +0300

    Update TMsgTest

--- TTalk/app-msg-tests/src/test/java/il/ac/technion/cs/sd/app/msg/TMsgTest.java
+++ TTalk/app-msg-tests/src/test/java/il/ac/technion/cs/sd/app/msg/TMsgTest.java
@@ -3,14 +3,16 @@ package il.ac.technion.cs.sd.app.msg;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Optional;
+import java.util.concurrent.*;
 
 import org.junit.*;
 
 public class TMsgTest {
-	private ServerMailApplication	server	= new ServerMailApplication("Server");
+	private ServerMailApplication			server				= new ServerMailApplication("Server");
 	// all listened to incoming messages will be written here
-	private Boolean					lastFriendshipReply;
-	private InstantMessage			lastMessage;
+	// a blocking queue is used to overcome threading issues
+	private BlockingQueue<Boolean>			friendshipReplies	= new LinkedBlockingQueue<>();
+	private BlockingQueue<InstantMessage>	messages			= new LinkedBlockingQueue<>();
 	
 	private ClientMsgApplication buildClient(String login) {
 		return new ClientMsgApplication(server.getAddress(), login);
@@ -30,16 +32,14 @@ public class TMsgTest {
 	@Test
 	public void basicTest() throws Exception {
 		ClientMsgApplication gal = buildClient("Gal");
-		gal.login(x -> lastMessage = x, x -> true, (x, y) -> lastFriendshipReply = y);
+		gal.login(x -> {}, x -> true, (x, y) -> friendshipReplies.add(y));
 		assertEquals(Optional.empty(), gal.isOnline("Itay")); // Itay isn't a friend
 		gal.sendMessage("Itay", "Hi");
 		ClientMsgApplication itay = buildClient("Itay");
-		itay.login(x -> lastMessage = x, x -> true, (x, y) -> lastFriendshipReply = y);
-		Thread.sleep(100);
-		assertEquals(lastMessage, new InstantMessage("Gal", "Itay", "Hi")); // Itay received the message as soon as he logged in
+		itay.login(x -> messages.add(x), x -> true, (x, y) -> {});
+		assertEquals(messages.take(), new InstantMessage("Gal", "Itay", "Hi")); // Itay received the message as soon as he logged in
 		gal.requestFriendship("Itay");
-		Thread.sleep(100);
-		assertEquals(true, lastFriendshipReply); // itay auto replies yes
+		assertEquals(true, friendshipReplies.take()); // itay auto replies yes
 		assertEquals(Optional.of(true), gal.isOnline("Itay")); // itay is a friend and is online
 		itay.logout();
 		assertEquals(Optional.of(false), gal.isOnline("Itay")); // itay is a friend and is offline
